c67ff8b73543a2b60d70e6f4135895cb009f365c
cricksmaidiene <easwaran.hari@gmail.com>
2022-03-28 20:38:35 +0400

    Update `setup.py` for Pypi

--- setup.py
+++ setup.py
@@ -4,9 +4,13 @@ setup(
     name="dotlas",
     version="1.0",
     description="Dotlas Python Package",
+    author="Dotlas Inc.",
+    author_email="tech@dotlas.com",
     url="https://github.com/dotlas/api-client-python",
     packages=find_packages(),
     python_requires=">=3.8",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
     install_requires=[
         "pydantic",
         "requests"
